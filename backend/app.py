49bbbaf73a54d82e5f1494eb3ffb167c34c6d04f
Atul <atul.tmsl@gmail.com>
2024-09-04 11:21:34 +0530

    configurations in app.py configurations in config.py

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,19 @@
-from flask import Flask, request, jsonify, session # type: ignore
-from flask_cors import CORS # type: ignore
-
+#flask bakend with react frontend
+from flask import *
+from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.json.get('username')
-    password = request.json.get('password')
-
-    # Check if the username and password are valid
-    if username == 'admin' and password == 'password':
-        session['logged_in'] = True
-        return jsonify({'message': 'Login successful'})
-    else:
-        return jsonify({'message': 'Invalid credentials'})
-
-@app.route('/logout', methods=['POST'])
-def logout():
-    session.pop('logged_in', None)
-    return jsonify({'message': 'Logout successful'})
-
-@app.route('/protected', methods=['GET'])
-def protected():
-    if 'logged_in' in session:
-        return jsonify({'message': 'This is a protected route'})
-    else:
-        return jsonify({'message': 'Unauthorized'})
+@app.route('/api')
+def api():
+    return jsonify({'data': 'Hello from the backend!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
+
+
+
---
+++ backend/config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    OAUTH2_CLIENT_ID = os.getenv("OAUTH2_CLIENT_ID")
+    OAUTH2_CLIENT_SECRET = os.getenv("OAUTH2_CLIENT_SECRET")
+    OAUTH2_META_URL = os.getenv("OAUTH2_META_URL")
+    FLASK_SECRET = os.getenv("FLASK_SECRET")
+    FLASK_PORT = 5000
+    YOUTUBE_API = os.getenv("apiKeyYoutube")
+    FIREBASE_CONFIG = {
+        'apiKey': os.getenv("API_KEY"),
+        'authDomain': os.getenv("AUTH_DOMAIN"),
+        'projectId': os.getenv("PROJECT_ID"),
+        'storageBucket': os.getenv("STORAGE_BUCKET"),
+        'messagingSenderId': os.getenv("MESSAGING_SENDER_ID"),
+        'appId': os.getenv("APP_ID"),
+        'measurementId': os.getenv("MEASUREMENT_ID"),
+        'databaseURL': ""
+    }
+    GEN_AI_API = os.getenv("API_KEY_GENAI")
