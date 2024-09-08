d194ddebc7115f41614e9fb46d71dc04137f6581
Kriti_Raj <ishakriti.2004@gmail.com>
2024-09-08 17:45:01 +0530

    Database correction done

--- backend/database.py
+++ backend/database.py
@@ -14,30 +14,31 @@ def get_connection():
             user="postgres", 
             password="3842"
     )
-
+    return g.db
 
 def generate_uuid():
     return str(uuid.uuid4())
 
 
 def current_timestamp():
-    return datetime.datetime.now()
+    return datetime.now()
 
 
 #Table1-Dep_heads
 
 # Create a Dep Head
-def create_dep_head(name, contact, email, dep_id=None):
+def create_dep_head(head_name, contact, email, pro_pic=None, dep_id=None):
     query = """
-    INSERT INTO dep_heads (head_id, name, contact, email, dep_id, created_at, updated_at)
-    VALUES (%s, %s, %s, %s, %s, %s)
-    RETURNING head_id;
+    INSERT INTO dep_heads (head_name, contact, email, pro_pic, dep_id, created_at, updated_at)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    RETURNING email;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (generate_uuid(), name, contact, email, dep_id, current_timestamp(), current_timestamp()))
+        cursor.execute(query, (head_name, contact, email, pro_pic, dep_id, current_timestamp(), current_timestamp()))
         conn.commit()
         return cursor.fetchone()[0]
+
 
 # Read Dep Heads
 def read_dep_heads():
@@ -47,28 +48,30 @@ def read_dep_heads():
         cursor.execute(query)
         return cursor.fetchall()
 
+
 # Update Dep Head
-def update_dep_head(head_id, name=None, contact=None, email=None, dep_id=None):
+def update_dep_head(email, head_name=None, contact=None, pro_pic=None, dep_id=None):
     query = """
     UPDATE dep_heads
-    SET name = COALESCE(%s, name),
+    SET head_name = COALESCE(%s, head_name),
         contact = COALESCE(%s, contact),
-        email = COALESCE(%s, email),
+        pro_pic = COALESCE(%s, pro_pic),
         dep_id = COALESCE(%s, dep_id),
         updated_at = %s
-    WHERE head_id = %s;
+    WHERE email = %s;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (name, contact, email, dep_id, current_timestamp(), head_id))
+        cursor.execute(query, (head_name, contact, pro_pic, dep_id, current_timestamp(), email))
         conn.commit()
 
+
 # Delete Dep Head
-def delete_dep_head(head_id):
-    query = "DELETE FROM dep_heads WHERE head_id = %s"
+def delete_dep_head(email):
+    query = "DELETE FROM dep_heads WHERE email = %s"
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (head_id,))
+        cursor.execute(query, (email,))
         conn.commit()
 
 
@@ -77,15 +80,16 @@ def delete_dep_head(head_id):
 # Create a Department
 def create_department(dep_name, loc, contact, description=None):
     query = """
-    INSERT INTO departments (dep_id, dep_name, loc, contact, description, created_at, updated_at)
+    INSERT INTO departments (dep_name, loc, contact, description, created_at, updated_at)
     VALUES (%s, %s, %s, %s, %s, %s)
     RETURNING dep_id;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (generate_uuid(), dep_name, loc, contact, description, current_timestamp(), current_timestamp()))
+        cursor.execute(query, (dep_name, loc, contact, description, current_timestamp(), current_timestamp()))
         conn.commit()
         return cursor.fetchone()[0]
+
 
 # Read Departments
 def read_departments():
@@ -94,6 +98,7 @@ def read_departments():
     with conn.cursor(cursor_factory=RealDictCursor) as cursor:
         cursor.execute(query)
         return cursor.fetchall()
+
 
 # Update Department
 def update_department(dep_id, dep_name=None, loc=None, contact=None, description=None):
@@ -120,19 +125,21 @@ def delete_department(dep_id):
         conn.commit()
 
 
+
 #Table3-employees
 # Create an Employee
 def create_employee(emp_name, cont, email, role, dep_id=None):
     query = """
-    INSERT INTO employees (emp_id, emp_name, cont, email, role, dep_id, created_at, updated_at)
+    INSERT INTO employees (emp_name, cont, email, role, dep_id, created_at, updated_at)
     VALUES (%s, %s, %s, %s, %s, %s, %s)
     RETURNING emp_id;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (generate_uuid(), emp_name, cont, email, role, dep_id, current_timestamp(), current_timestamp()))
+        cursor.execute(query, (emp_name, cont, email, role, dep_id, current_timestamp(), current_timestamp()))
         conn.commit()
         return cursor.fetchone()[0]
+
 
 # Read Employees
 def read_employees():
@@ -141,6 +148,7 @@ def read_employees():
     with conn.cursor(cursor_factory=RealDictCursor) as cursor:
         cursor.execute(query)
         return cursor.fetchall()
+
 
 # Update Employee
 def update_employee(emp_id, emp_name=None, cont=None, email=None, role=None, dep_id=None):
@@ -168,19 +176,21 @@ def delete_employee(emp_id):
         conn.commit()
 
 
+
 #Table4-engineers
 # Create an Engineer
-def create_engineer(eng_name):
+def create_engineer(eng_name, pro_pic=None):
     query = """
-    INSERT INTO engineers (eng_id, eng_name)
-    VALUES (%s, %s)
+    INSERT INTO engineers (eng_name, pro_pic, created_at, updated_at)
+    VALUES (%s, %s, %s, %s)
     RETURNING eng_id;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (generate_uuid(), eng_name))
+        cursor.execute(query, (eng_name, pro_pic, current_timestamp(), current_timestamp()))
         conn.commit()
         return cursor.fetchone()[0]
+
 
 # Read Engineers
 def read_engineers():
@@ -190,17 +200,21 @@ def read_engineers():
         cursor.execute(query)
         return cursor.fetchall()
 
+
 # Update Engineer
-def update_engineer(eng_id, eng_name=None):
+def update_engineer(eng_id, eng_name=None, pro_pic=None):
     query = """
     UPDATE engineers
-    SET eng_name = COALESCE(%s, eng_name)
+    SET eng_name = COALESCE(%s, eng_name),
+        pro_pic = COALESCE(%s, pro_pic),
+        updated_at = %s
     WHERE eng_id = %s;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (eng_name, eng_id))
+        cursor.execute(query, (eng_name, pro_pic, current_timestamp(), eng_id))
         conn.commit()
+
 
 # Delete Engineer
 def delete_engineer(eng_id):
@@ -211,19 +225,21 @@ def delete_engineer(eng_id):
         conn.commit()
 
 
+
 #Table5-Project_managers
 # Create a Project Manager
-def create_project_manager(pm_name):
+def create_project_manager(pm_name, pro_pic=None):
     query = """
-    INSERT INTO project_managers (pm_id, pm_name)
-    VALUES (%s, %s)
+    INSERT INTO project_managers (pm_name, pro_pic, created_at, updated_at)
+    VALUES (%s, %s, %s, %s)
     RETURNING pm_id;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (generate_uuid(), pm_name))
+        cursor.execute(query, (pm_name, pro_pic, current_timestamp(), current_timestamp()))
         conn.commit()
         return cursor.fetchone()[0]
+
 
 # Read Project Managers
 def read_project_managers():
@@ -233,16 +249,19 @@ def read_project_managers():
         cursor.execute(query)
         return cursor.fetchall()
 
+
 # Update Project Manager
-def update_project_manager(pm_id, pm_name=None):
+def update_project_manager(pm_id, pm_name=None, pro_pic=None):
     query = """
     UPDATE project_managers
-    SET pm_name = COALESCE(%s, pm_name)
+    SET pm_name = COALESCE(%s, pm_name),
+        pro_pic = COALESCE(%s, pro_pic),
+        updated_at = %s
     WHERE pm_id = %s;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (pm_name, pm_id))
+        cursor.execute(query, (pm_name, pro_pic, current_timestamp(), pm_id))
         conn.commit()
 
 # Delete Project Manager
@@ -254,19 +273,21 @@ def delete_project_manager(pm_id):
         conn.commit()
 
 
+
 #Table6-Public_users
 # Create a Public User
-def create_public_user(uname, email, contact=None, comp=None, stat="Pending"):
+def create_public_user(uname, email, contact=None, complain=None, pro_pic=None, stat='Pending'):
     query = """
-    INSERT INTO public_users (id, uname, email, contact, comp, stat, created_at, updated_at)
+    INSERT INTO public_users (uname, email, contact, complain, pro_pic, stat, created_at, updated_at)
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-    RETURNING id;
+    RETURNING email;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (generate_uuid(), uname, email, contact, comp, stat, current_timestamp(), current_timestamp()))
+        cursor.execute(query, (uname, email, contact, complain, pro_pic, stat, current_timestamp(), current_timestamp()))
         conn.commit()
         return cursor.fetchone()[0]
+
 
 # Read Public Users
 def read_public_users():
@@ -276,45 +297,49 @@ def read_public_users():
         cursor.execute(query)
         return cursor.fetchall()
 
+
 # Update Public User
-def update_public_user(user_id, uname=None, email=None, contact=None, comp=None, stat=None):
+def update_public_user(email, uname=None, contact=None, complain=None, pro_pic=None, stat=None):
     query = """
     UPDATE public_users
     SET uname = COALESCE(%s, uname),
-        email = COALESCE(%s, email),
         contact = COALESCE(%s, contact),
-        comp = COALESCE(%s, comp),
+        complain = COALESCE(%s, complain),
+        pro_pic = COALESCE(%s, pro_pic),
         stat = COALESCE(%s, stat),
         updated_at = %s
-    WHERE id = %s;
+    WHERE email = %s;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (uname, email, contact, comp, stat, current_timestamp(), user_id))
+        cursor.execute(query, (uname, contact, complain, pro_pic, stat, current_timestamp(), email))
         conn.commit()
 
+
 # Delete Public User
-def delete_public_user(user_id):
-    query = "DELETE FROM public_users WHERE id = %s"
+def delete_public_user(email):
+    query = "DELETE FROM public_users WHERE email = %s"
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (user_id,))
+        cursor.execute(query, (email,))
         conn.commit()
+
 
 
 #Table7-tasks
 # Create a Task
-def create_task(title, descr, assign_to=None, dept_id=None, stat="Pending", loc=None, due=None):
+def create_task(title, descr=None, assign_to=None, dept_id=None, stat='Pending', priority='Medium', loc=None, due=None, req=None):
     query = """
-    INSERT INTO tasks (t_id, title, descr, assign_to, dept_id, stat, loc, due, created_at, updated_at)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO tasks (title, descr, assign_to, dept_id, stat, priority, loc, due, req, created_at, updated_at)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     RETURNING t_id;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (generate_uuid(), title, descr, assign_to, dept_id, stat, loc, due, current_timestamp(), current_timestamp()))
+        cursor.execute(query, (title, descr, assign_to, dept_id, stat, priority, loc, due, req, current_timestamp(), current_timestamp()))
         conn.commit()
         return cursor.fetchone()[0]
+
 
 # Read Tasks
 def read_tasks():
@@ -324,8 +349,9 @@ def read_tasks():
         cursor.execute(query)
         return cursor.fetchall()
 
+
 # Update Task
-def update_task(t_id, title=None, descr=None, assign_to=None, dept_id=None, stat=None, loc=None, due=None):
+def update_task(t_id, title=None, descr=None, assign_to=None, dept_id=None, stat=None, priority=None, loc=None, due=None, req=None):
     query = """
     UPDATE tasks
     SET title = COALESCE(%s, title),
@@ -333,15 +359,18 @@ def update_task(t_id, title=None, descr=None, assign_to=None, dept_id=None, stat
         assign_to = COALESCE(%s, assign_to),
         dept_id = COALESCE(%s, dept_id),
         stat = COALESCE(%s, stat),
+        priority = COALESCE(%s, priority),
         loc = COALESCE(%s, loc),
         due = COALESCE(%s, due),
+        req = COALESCE(%s, req),
         updated_at = %s
     WHERE t_id = %s;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (title, descr, assign_to, dept_id, stat, loc, due, current_timestamp(), t_id))
+        cursor.execute(query, (title, descr, assign_to, dept_id, stat, priority, loc, due, req, current_timestamp(), t_id))
         conn.commit()
+
 
 # Delete Task
 def delete_task(t_id):
@@ -354,15 +383,15 @@ def delete_task(t_id):
 
 #Table8-inventory
 # Create an Inventory Item
-def create_inventory_item(in_name, qty, descr=None, dep_id=None):
+def create_inventory(in_name, qty, descr=None, dep_id=None):
     query = """
-    INSERT INTO inventory (in_id, in_name, qty, descr, dep_id, created_at, updated_at)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO inventory (in_name, qty, descr, dep_id, created_at, updated_at)
+    VALUES (%s, %s, %s, %s, %s, %s)
     RETURNING in_id;
     """
     conn = get_connection()
     with conn.cursor() as cursor:
-        cursor.execute(query, (generate_uuid(), in_name, qty, descr, dep_id, current_timestamp(), current_timestamp()))
+        cursor.execute(query, (in_name, qty, descr, dep_id, current_timestamp(), current_timestamp()))
         conn.commit()
         return cursor.fetchone()[0]
 
@@ -374,8 +403,9 @@ def read_inventory():
         cursor.execute(query)
         return cursor.fetchall()
 
+
 # Update Inventory Item
-def update_inventory_item(in_id, in_name=None, qty=None, descr=None, dep_id=None):
+def update_inventory(in_id, in_name=None, qty=None, descr=None, dep_id=None):
     query = """
     UPDATE inventory
     SET in_name = COALESCE(%s, in_name),
@@ -390,13 +420,15 @@ def update_inventory_item(in_id, in_name=None, qty=None, descr=None, dep_id=None
         cursor.execute(query, (in_name, qty, descr, dep_id, current_timestamp(), in_id))
         conn.commit()
 
+
 # Delete Inventory Item
-def delete_inventory_item(in_id):
+def delete_inventory(in_id):
     query = "DELETE FROM inventory WHERE in_id = %s"
     conn = get_connection()
     with conn.cursor() as cursor:
         cursor.execute(query, (in_id,))
         conn.commit()
+
 
 
 def close_database(e=None):
